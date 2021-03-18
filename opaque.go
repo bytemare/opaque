5b0b011a9ffa377dd8675eb751799dfdaa8dbe49
bytemare <{ID}+{username}@users.noreply.github.com>
2021-03-18 19:17:25 +0100

    update parameter serialization

--- opaque.go
+++ opaque.go
@@ -21,13 +21,20 @@ type Parameters struct {
 	Hash            hash.Hashing           `json:"hash"`
 	MHF             mhf.Identifier         `json:"mhf"`
 	Mode            envelope.Mode          `json:"mode"`
-	Ake             string                 `json:"ake"`
 	Group           ciphersuite.Identifier `json:"group"`
 	NonceLen        int                    `json:"nn"`
 }
 
 func (p *Parameters) Serialize() []byte {
-	return utils.Concatenate(0, []byte{byte(p.OprfCiphersuite)}, []byte{byte(p.Mode)}, []byte{byte(p.Hash)}, encoding.I2OSP(p.NonceLen, 1))
+	return utils.Concatenate(0,
+		[]byte{byte(p.OprfCiphersuite)},
+		[]byte{byte(p.KDF)},
+		[]byte{byte(p.MAC)},
+		[]byte{byte(p.Hash)},
+		[]byte{byte(p.MHF)},
+		[]byte{byte(p.Mode)},
+		[]byte{byte(p.Group)},
+		encoding.I2OSP(p.NonceLen, 1))
 }
 
 func (p *Parameters) Client() *Client {
@@ -38,7 +45,7 @@ func (p *Parameters) Server() *Server {
 	return NewServer(p)
 }
 
-func (p *Parameters) Deserializer() *message.Deserializer {
+func (p *Parameters) MessageDeserializer() *message.Deserializer {
 	return &message.Deserializer{
 		OPRFPointLength: p.OprfCiphersuite.Group(),
 		AkeGroup:        p.Group,
@@ -48,18 +55,23 @@ func (p *Parameters) Deserializer() *message.Deserializer {
 }
 
 func (p *Parameters) String() string {
-	return fmt.Sprintf("%s-%s", p.OprfCiphersuite, p.Hash)
+	return fmt.Sprintf("%s-%s-%s-%s-%s-%v-%s-%d",
+		p.OprfCiphersuite, p.KDF, p.MAC, p.Hash, p.MHF, p.Mode, p.Group, p.NonceLen)
 }
 
 func DeserializeParameters(encoded []byte) (*Parameters, error) {
-	if len(encoded) != 4 {
+	if len(encoded) != 8 {
 		return nil, errors.New("invalid length")
 	}
 
 	return &Parameters{
 		OprfCiphersuite: voprf.Ciphersuite(encoded[0]),
-		Mode:            envelope.Mode(encoded[1]),
-		Hash:            hash.Hashing(encoded[2]),
-		NonceLen:        int(encoded[3]),
+		KDF:             hash.Hashing(encoded[1]),
+		MAC:             hash.Hashing(encoded[2]),
+		Hash:            hash.Hashing(encoded[3]),
+		MHF:             mhf.Identifier(encoded[4]),
+		Mode:            envelope.Mode(encoded[5]),
+		Group:           ciphersuite.Identifier(6),
+		NonceLen:        encoding.OS2IP(encoded[7:]),
 	}, nil
 }
