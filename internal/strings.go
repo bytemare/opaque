07a2345b0079f09195a1e9d455abcbb3b55e10a4
bytemare <{ID}+{username}@users.noreply.github.com>
2021-04-25 01:41:53 +0200

    add hardcoded strings

---
+++ internal/strings.go
@@ -0,0 +1,38 @@
+package internal
+
+const (
+	// Envelope
+
+	TagAuthKey    = "AuthKey"
+	TagExportKey  = "ExportKey"
+	TagMaskingKey = "MaskingKey"
+
+	// Internal Mode
+
+	H2sDST = "OPAQUE-HashToScalar"
+	SkDST  = "PrivateKey"
+
+	// External Mode
+
+	TagPad = "Pad"
+
+	// 3DH
+
+	Tag3DH        = "3DH"
+	LabelPrefix   = "OPAQUE-"
+	TagHandshake  = "HandshakeSecret"
+	TagSession    = "SessionKey"
+	TagMacServer  = "ServerMAC"
+	TagMacClient  = "ClientMAC"
+	TagEncServer  = "HandshakeKey"
+	EncryptionTag = "EncryptionPad"
+
+	// Client
+
+	TagCredentialResponsePad = "CredentialResponsePad"
+
+	// Server
+
+	OprfKey               = "OprfKey"
+)
+
