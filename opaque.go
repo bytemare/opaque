2f7c327968a09eb918de9b13dd57d88c6a4f9aa7
bytemare <3641580+bytemare@users.noreply.github.com>
2021-05-12 12:36:07 +0200

    harmonize parameter usage and clean up a bit

--- opaque.go
+++ opaque.go
@@ -4,15 +4,13 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/bytemare/cryptotools/group/ciphersuite"
 	"github.com/bytemare/cryptotools/hash"
 	"github.com/bytemare/cryptotools/mhf"
-	"github.com/bytemare/cryptotools/utils"
 	"github.com/bytemare/voprf"
 
 	"github.com/bytemare/opaque/internal"
-	"github.com/bytemare/opaque/internal/core/envelope"
 	"github.com/bytemare/opaque/internal/encoding"
+	"github.com/bytemare/opaque/internal/envelope"
 	"github.com/bytemare/opaque/message"
 )
 
@@ -27,48 +25,81 @@ const (
 	External
 )
 
-// Ciphersuite identifies the OPRF compatible cipher suite to be used.
-type Ciphersuite voprf.Ciphersuite
+// Group identifies the prime-order group with hash-to-curve capability to use in OPRF and AKE.
+type Group byte
 
 const (
-	// RistrettoSha512 is the OPRF cipher suite of the Ristretto255 group and SHA-512.
-	RistrettoSha512 = Ciphersuite(voprf.RistrettoSha512)
+	// RistrettoSha512 identifies the Ristretto255 group and SHA-512.
+	RistrettoSha512 = Group(voprf.RistrettoSha512)
 
-	// P256Sha256 is the OPRF cipher suite of the NIST P-256 group and SHA-256.
-	P256Sha256 = Ciphersuite(voprf.P256Sha256)
+	// decaf448Shake256 identifies the Decaf448 group and Shake-256.
+	// decaf448Shake256 = 2.
 
-	// P384Sha512 is the OPRF cipher suite of the NIST P-384 group and SHA-512.
-	P384Sha512 = Ciphersuite(voprf.P384Sha512)
+	// P256Sha256 identifies the NIST P-256 group and SHA-256.
+	P256Sha256 = Group(voprf.P256Sha256)
 
-	// P521Sha512 is the OPRF cipher suite of the NIST P-512 group and SHA-512.
-	P521Sha512 = Ciphersuite(voprf.P521Sha512)
+	// P384Sha512 identifies the NIST P-384 group and SHA-512.
+	P384Sha512 = Group(voprf.P384Sha512)
+
+	// P521Sha512 identifies the NIST P-512 group and SHA-512.
+	P521Sha512 = Group(voprf.P521Sha512)
 )
 
-// CredentialIdentifier designates the server's internal unique identifier of the user entry.
-type CredentialIdentifier []byte
-
-// Configuration represents an OPAQUE configuration.
-type Configuration struct {
-	OprfCiphersuite Ciphersuite            `json:"oprf"`
-	KDF             hash.Hashing           `json:"kdf"`
-	MAC             hash.Hashing           `json:"mac"`
-	Hash            hash.Hashing           `json:"hash"`
-	MHF             mhf.Identifier         `json:"mhf"`
-	Mode            Mode                   `json:"mode"`
-	AKEGroup        ciphersuite.Identifier `json:"group"`
-	NonceLen        int                    `json:"nn"`
+// String implements the Stringer() interface for the Group.
+func (g *Group) String() string {
+	return voprf.Ciphersuite(*g).String()
 }
 
-func (p *Configuration) toInternal() *internal.Parameters {
+// Credentials holds the client and server ids (will certainly disappear in next versions°.
+type Credentials struct {
+	Client, Server              []byte
+	TestEnvNonce, TestMaskNonce []byte
+}
+
+// Configuration represents an OPAQUE configuration. Note that OprfGroup and AKEGroup are recommended to be the same,
+// as well as KDF, MAC, Hash should be the same.
+type Configuration struct {
+	// OprfGroup identifies the OPRF ciphersuite to be used.
+	OprfGroup Group `json:"oprf"`
+
+	// KDF identifies the hash function to be used for key derivation (e.g. HKDF).
+	// Identifiers are defined in github.com/bytemare/cryptotools/hash.
+	KDF hash.Hashing `json:"kdf"`
+
+	// MAC identifies the hash function to be used for message authentication (e.g. HMAC).
+	// Identifiers are defined in github.com/bytemare/cryptotools/hash.
+	MAC hash.Hashing `json:"mac"`
+
+	// Hash identifies the hash function to be used for hashing, as defined in github.com/bytemare/cryptotools/hash.
+	Hash hash.Hashing `json:"hash"`
+
+	// MHF identifies the memory-hard function for expensive key derivation on the client,
+	// defined in github.com/bytemare/cryptotools/mhf.
+	MHF mhf.Identifier `json:"mhf"`
+
+	// Mode identifies the envelope mode to be used.
+	Mode Mode `json:"mode"`
+
+	// AKEGroup identifies the prime-order group to use in the AKE.
+	AKEGroup Group `json:"group"`
+
+	// NonceLen identifies the length to use for nonces. 32 is the recommended value.
+	NonceLen int `json:"nn"`
+}
+
+func (c *Configuration) toInternal() *internal.Parameters {
+	g := voprf.Ciphersuite(c.OprfGroup)
+	cs := g.Group()
+
 	ip := &internal.Parameters{
-		OprfCiphersuite: voprf.Ciphersuite(p.OprfCiphersuite),
-		KDF:             &internal.KDF{H: p.KDF.Get()},
-		MAC:             &internal.Mac{H: p.MAC.Get()},
-		Hash:            &internal.Hash{H: p.Hash.Get()},
-		MHF:             &internal.MHF{MHF: p.MHF.Get()},
-		AKEGroup:        p.AKEGroup,
-		NonceLen:        p.NonceLen,
-		EnvelopeSize:    envelope.Size(envelope.Mode(p.Mode), p.NonceLen, p.MAC.Size(), p.AKEGroup),
+		OprfCiphersuite: g,
+		KDF:             &internal.KDF{H: c.KDF.Get()},
+		MAC:             &internal.Mac{H: c.MAC.Get()},
+		Hash:            &internal.Hash{H: c.Hash.Get()},
+		MHF:             &internal.MHF{MHF: c.MHF.Get()},
+		AKEGroup:        cs,
+		NonceLen:        c.NonceLen,
+		EnvelopeSize:    envelope.Size(envelope.Mode(c.Mode), c.NonceLen, c.MAC.Size(), cs),
 	}
 	ip.Init()
 
@@ -76,34 +107,34 @@ func (p *Configuration) toInternal() *internal.Parameters {
 }
 
 // Serialize returns the byte encoding of the Configuration structure.
-func (p *Configuration) Serialize() []byte {
+func (c *Configuration) Serialize() []byte {
 	b := make([]byte, 8)
-	b[0] = byte(p.OprfCiphersuite)
-	b[1] = byte(p.KDF)
-	b[2] = byte(p.MAC)
-	b[3] = byte(p.Hash)
-	b[4] = byte(p.MHF)
-	b[5] = byte(p.Mode)
-	b[6] = byte(p.AKEGroup)
-	b[7] = encoding.I2OSP(p.NonceLen, 1)[0]
+	b[0] = byte(c.OprfGroup)
+	b[1] = byte(c.KDF)
+	b[2] = byte(c.MAC)
+	b[3] = byte(c.Hash)
+	b[4] = byte(c.MHF)
+	b[5] = byte(c.Mode)
+	b[6] = byte(c.AKEGroup)
+	b[7] = encoding.I2OSP(c.NonceLen, 1)[0]
 
 	return b
 }
 
 // Client returns a newly instantiated Client from the Configuration.
-func (p *Configuration) Client() *Client {
-	return NewClient(p)
+func (c *Configuration) Client() *Client {
+	return NewClient(c)
 }
 
 // Server returns a newly instantiated Server from the Configuration.
-func (p *Configuration) Server() *Server {
-	return NewServer(p)
+func (c *Configuration) Server() *Server {
+	return NewServer(c)
 }
 
 // String returns a string representation of the parameter set.
-func (p *Configuration) String() string {
+func (c *Configuration) String() string {
 	return fmt.Sprintf("%s-%s-%s-%s-%s-%v-%s-%d",
-		voprf.Ciphersuite(p.OprfCiphersuite), p.KDF, p.MAC, p.Hash, p.MHF, p.Mode, p.AKEGroup, p.NonceLen)
+		c.OprfGroup.String(), c.KDF, c.MAC, c.Hash, c.MHF, c.Mode, c.AKEGroup.String(), c.NonceLen)
 }
 
 var errInvalidLength = errors.New("invalid length")
@@ -116,66 +147,37 @@ func DeserializeConfiguration(encoded []byte) (*Configuration, error) {
 	}
 
 	return &Configuration{
-		OprfCiphersuite: Ciphersuite(encoded[0]),
-		KDF:             hash.Hashing(encoded[1]),
-		MAC:             hash.Hashing(encoded[2]),
-		Hash:            hash.Hashing(encoded[3]),
-		MHF:             mhf.Identifier(encoded[4]),
-		Mode:            Mode(encoded[5]),
-		AKEGroup:        ciphersuite.Identifier(6),
-		NonceLen:        encoding.OS2IP(encoded[7:]),
+		OprfGroup: Group(encoded[0]),
+		KDF:       hash.Hashing(encoded[1]),
+		MAC:       hash.Hashing(encoded[2]),
+		Hash:      hash.Hashing(encoded[3]),
+		MHF:       mhf.Identifier(encoded[4]),
+		Mode:      Mode(encoded[5]),
+		AKEGroup:  Group(encoded[6]),
+		NonceLen:  encoding.OS2IP(encoded[7:]),
 	}, nil
 }
 
 // DefaultConfiguration returns a default configuration with strong parameters.
 func DefaultConfiguration() *Configuration {
 	return &Configuration{
-		OprfCiphersuite: RistrettoSha512,
-		KDF:             hash.SHA512,
-		MAC:             hash.SHA512,
-		Hash:            hash.SHA512,
-		MHF:             mhf.Scrypt,
-		Mode:            Internal,
-		AKEGroup:        ciphersuite.Ristretto255Sha512,
-		NonceLen:        32,
+		OprfGroup: RistrettoSha512,
+		KDF:       hash.SHA512,
+		MAC:       hash.SHA512,
+		Hash:      hash.SHA512,
+		MHF:       mhf.Scrypt,
+		Mode:      Internal,
+		AKEGroup:  RistrettoSha512,
+		NonceLen:  32,
 	}
 }
 
 // ClientRecord is a server-side structure enabling the storage of user relevant information.
 type ClientRecord struct {
-	CredentialIdentifier
-	ClientIdentity []byte
+	CredentialIdentifier []byte
+	ClientIdentity       []byte
 	*message.RegistrationUpload
-}
 
-// Serialize returns the byte encoding of the ClientRecord.
-func (c *ClientRecord) Serialize() []byte {
-	return utils.Concatenate(0,
-		encoding.EncodeVector(c.CredentialIdentifier), encoding.EncodeVector(c.ClientIdentity), c.RegistrationUpload.Serialize())
-}
-
-// DeserializeClientRecord decodes the input ClientRecord given the application parameters.
-func DeserializeClientRecord(encoded []byte, p *internal.Parameters) (*ClientRecord, error) {
-	ci, offset1, err := encoding.DecodeVector(encoded)
-	if err != nil {
-		return nil, fmt.Errorf("decoding credential identifier: %w", err)
-	}
-
-	idc, offset2, err := encoding.DecodeVector(encoded[offset1:])
-	if err != nil {
-		return nil, fmt.Errorf("decoding client identifier: %w", err)
-	}
-
-	p.Init()
-
-	upload, err := p.DeserializeRegistrationUpload(encoded[offset1+offset2:])
-	if err != nil {
-		return nil, fmt.Errorf("decoding client upload: %w", err)
-	}
-
-	return &ClientRecord{
-		CredentialIdentifier: ci,
-		ClientIdentity:       idc,
-		RegistrationUpload:   upload,
-	}, nil
+	// testing
+	TestMaskNonce []byte
 }
