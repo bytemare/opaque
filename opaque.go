32b403b3830cc3059f81c30dc7d879c50e81d487
bytemare <3641580+bytemare@users.noreply.github.com>
2021-05-17 01:46:36 +0200

    update go doc

--- opaque.go
+++ opaque.go
@@ -1,3 +1,10 @@
+// Package opaque implements the OPAQUE asymmetric password-authenticated key exchange protocol.
+//
+// OPAQUE is an asymmetric Password Authenticated Key Exchange (PAKE).
+//
+// This package implements the official OPAQUE definition. For protocol details, please refer to the IETF protocol
+// document [here](https://datatracker.ietf.org/doc/draft-irtf-cfrg-opaque).
+//
 package opaque
 
 import (
