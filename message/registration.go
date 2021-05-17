23070728b783a6caa04a3c69a9ec0b35a5f51024
bytemare <3641580+bytemare@users.noreply.github.com>
2021-05-17 02:26:37 +0200

    update Licence

--- message/registration.go
+++ message/registration.go
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (C) 2020 Daniel Bourdrez. All Rights Reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree or at
+// https://spdx.org/licenses/MIT.html
+
 // Package message provides message structures for the OPAQUE protocol.
 package message
 
