cfa68e1d49a38fd7b81fc15e7398938c2891f14a
bytemare <3641580+bytemare@users.noreply.github.com>
2021-05-19 16:09:46 +0200

    update link in doc

--- opaque.go
+++ opaque.go
@@ -11,7 +11,7 @@
 // OPAQUE is an asymmetric Password Authenticated Key Exchange (PAKE).
 //
 // This package implements the official OPAQUE definition. For protocol details, please refer to the IETF protocol
-// document [here](https://datatracker.ietf.org/doc/draft-irtf-cfrg-opaque).
+// document at https://datatracker.ietf.org/doc/draft-irtf-cfrg-opaque.
 //
 package opaque
 
