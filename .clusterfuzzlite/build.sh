010eca8225ca2fa3487ad9418a9379ca482b9892
Daniel Bourdrez <3641580+bytemare@users.noreply.github.com>
2024-10-05 18:38:53 +0200

    test
    Signed-off-by: Daniel Bourdrez <3641580+bytemare@users.noreply.github.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -9,4 +9,4 @@ set -ex
 # compile_go_fuzzer github.com/bytemare/opaque FuzzDeserializeKE1 Fuzz_DeserializeKE1 fuzz
 # compile_go_fuzzer github.com/bytemare/opaque FuzzDeserializeKE2 Fuzz_DeserializeKE2 fuzz
 # compile_go_fuzzer github.com/bytemare/opaque FuzzDeserializeKE3 Fuzz_DeserializeKE3 fuzz
-compile_go_fuzzer github.com/bytemare/opaque FuzzKE3 Fuzz_DeserializeKE3 fuzz
+compile_go_fuzzer github.com/bytemare/opaque/opaque_test FuzzKE3 Fuzz_DeserializeKE3 fuzz
